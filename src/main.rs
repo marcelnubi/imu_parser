732e72d3464579a304077aa48588a499e0036b9e
Marcel Mello <mello.marcel@gmail.com>
2019-04-23 10:12:59 -0300

    Adicionando magnetometro

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,13 @@ use std::io::Read;
 use std::io::Write;
 
 fn main() -> io::Result<()> {
-    let f = File::open("daq_imu_0_pkpk35_0.2hz.log")?;
+    let f = File::open("daq_imu_pkpk60_0_0_0.2hz.log")?;
     let mut buf_reader = BufReader::new(f);
 
     let wf = File::create("out.csv")?;
     let mut buf_writer = BufWriter::new(wf);
 
-    let mut float_buf = [0; 24];
+    let mut float_buf = [0; 36];
     let mut done = false;
 
     writeln!(&mut buf_writer, "ax;ay;az;d_row;d_pitch;d_yaw")?;
@@ -29,15 +29,18 @@ fn main() -> io::Result<()> {
         let drow = LittleEndian::read_f32(&float_buf[12..16]) / 100.0;
         let dpitch = LittleEndian::read_f32(&float_buf[16..20]) / 100.0;
         let dyaw = LittleEndian::read_f32(&float_buf[20..24]) / 100.0;
+        let mx = LittleEndian::read_f32(&float_buf[24..28]) / 100.0;
+        let my = LittleEndian::read_f32(&float_buf[28..32]) / 100.0;
+        let mz = LittleEndian::read_f32(&float_buf[32..36]) / 100.0;
 
         println!(
-            "Ax={:.5} Ay={:.5} Az={:.5} dRow={:.5} dPitch={:.5} dYaw={:.5}",
-            ax, ay, az, drow, dpitch, dyaw
+            "Ax={:.5} Ay={:.5} Az={:.5} dRow={:.5} dPitch={:.5} dYaw={:.5} mX={:.5} mY={:.5} mZ={:.5}",
+            ax, ay, az, drow, dpitch, dyaw, mx, my, mz
         );
         writeln!(
             &mut buf_writer,
-            "{:.5};{:.5};{:.5};{:.5};{:.5};{:.5}",
-            ax, ay, az, drow, dpitch, dyaw
+            "{:.5};{:.5};{:.5};{:.5};{:.5};{:.5};{:.5};{:.5};{:.5}",
+            ax, ay, az, drow, dpitch, dyaw, mx, my, mz
         )?;
     }
     Ok(())
